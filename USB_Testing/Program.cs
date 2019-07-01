639c0499bf32bc730ac2b38637bb18c8b9ee73e7
Alejandro Resendez <al.resendez.gro@gmail.com>
2019-07-01 17:43:10 -0500

    Change from continuous running to command-based style

--- USB_Testing/Program.cs
+++ USB_Testing/Program.cs
@@ -12,24 +12,39 @@ namespace USB_Testing
     // Author: EARG 
     // ARGE Software® is a Trademark of ARGE Technologies LLC
 
+
+    /*
+         USB Testing device Application as Command Console
+         list_devices for All Devices
+         list_removable for Only Removable Disk Drives
+         count_removable to display how many removable devices are attached to PC
+        
+         
+     */
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            bool exit = false;
+            // bool exit = false;
             string option;
             testing_usb TestUSB = new testing_usb();
+
+            /*
             Console.WriteLine("USB Testing device Application \n " +
                 "1. 'list_devices' for All Devices \n " +
                 "2. list_removable for Only Removable Disk Drives \n " +
-                "3. Help to show menu: ");
+                "3. Help to show menu: "); */
 
-            while (exit == false)
+            //while (exit == false) //Removing Continuous Loop
+
+            // Console.WriteLine("Enter Option: ");
+            try
             {
-                Console.WriteLine("Enter Option: ");
-                option = Console.ReadLine();
-               
-                if(option.ToLower() == "list_devices")
+                option = args[0];
+
+                if (option.ToLower() == "list_devices")
                 {
                     TestUSB.list_devices();
                 }
@@ -47,26 +62,58 @@ namespace USB_Testing
                 }
                 if (option.ToLower() == "copy_read_test") //Will pick a random device and will try to copy and read back a file
                 {
-                    string filename_source, filename_dest;
-                    Console.WriteLine("Enter filename: ");
-                    filename_source = Console.ReadLine();
-                    Console.WriteLine("Enter filename: ");
-                    filename_dest = Console.ReadLine();
-                    TestUSB.File_CopyRead_Test(filename_source, filename_dest);
+                    string filename_source;
+
+                    // Console.WriteLine("Enter filename: ");
+                    try
+                    {
+                        filename_source = args[1];
+                        if (args[1] != null)
+                        {
+                            int result = TestUSB.File_WR_Test(filename_source);
+                            if (result != 0)
+                                Console.WriteLine("Operation Finished with ERROR code: {0}", result);
+                        }
+                        else
+                            Console.WriteLine("Please enter the Test filename [EX]: USBTest.exe copy_read_test c:\\image\\testimage.jpg");
+                    }
+                    catch (IndexOutOfRangeException no_arg)
+                    {
+                        Console.WriteLine("Please enter the Test filename [Example]: USBTest.exe copy_read_test c:\\image\\testimage.jpg");
+                        Console.WriteLine(no_arg.Message);
+                    }
                 }
                 if (option.ToLower() == "read_test")
                 {
                     //TestUSB.list_removable();
                 }
 
-                if (option == "exit")
+                if (option == "help" || option == null)
                 {
-                    exit = true;
-                
+                    Console.Write("Enter argument option USBTesting [option] [option] to execute USB Automated Test: " +
+                        "1. list_devices To list All drives" +
+                        "2. list_removable_parsable to display info of removable devices" +
+                        "3. copy_read_test [filename.extension] to copy, read and delete file test" +
+                        "4. help to display this help message");
+                    // exit = true;
+
                 }
 
+
             }
+            catch (IndexOutOfRangeException e)
+            {
+               
+                    Console.Write("Enter argument option USBTesting [option] [option] to execute USB Automated Test: \n" +
+                        "1. list_devices To list All drives \n" +
+                        "2. list_removable_parsable to display info of removable devices \n" +
+                        "3. copy_read_test [filename.extension] to copy, read and delete file test \n" +
+                        "4. help to display this help message \n\n");
+                    // exit = true;
             }
+
+            }
+           
 
     }
 }
--- USB_Testing/USB_Testing.cs
+++ USB_Testing/USB_Testing.cs
@@ -14,6 +14,8 @@ namespace USB_Testing
 
         public string dest_filename;
         public string source_filename;
+        public string[] USB3_Drives = new string[10];
+        public string[] USB2_Drives = new string[10];
 
         public testing_usb()
         {
@@ -105,35 +107,127 @@ namespace USB_Testing
         }
 
 
-        public void File_CopyRead_Test(string source_filename, string dest_filename)
+        public int File_WR_Test(string source_filename)
         {
+            string dest_filename_USB2 = "E:\\testfile.bin";
+            string dest_filename_USB3 = "F:\\testfile.bin";
             double data_transfer = 0;
+            int i = 0;
+            int j=0;
             //Get a random USB Drive
-            try
-            {
-                
+
+            // Pick a random USB 3.0 Drive
+            int usb2_drive = 0;
+            // Pick a random USB 2.0 drive
+            int usb3_drive = 0;
+
+                foreach (DriveInfo d in available_drives)
+                {
+                    if (d.VolumeLabel.ToString().IndexOf("MOTOUS") != -1)
+                    {
+                        this.USB3_Drives[i] = d.Name;
+                        i++;
+                    }
+                    if (d.VolumeLabel.ToString().IndexOf("_USB2") != -1)
+                    {
+                        this.USB2_Drives[j] = d.Name;
+                        j = j+1;
+                    } 
+
+                }
+
+            try //Write a file in the USB 2.0 drive and log the speed
+             {
+                //int RandomDrive = RandomNumber(0, 11);
+                dest_filename_USB2 = Path.Combine(this.USB2_Drives[usb2_drive], "TestFile.bin"); 
                 Stopwatch stopWatch = new Stopwatch();
                 stopWatch.Start();
-                File.Copy(source_filename, dest_filename); //Copy the file
+                File.Copy(source_filename, dest_filename_USB2); //Copy the file to USB2.0 drive
                 stopWatch.Stop();
                 // Get the elapsed time as a TimeSpan value.
                 TimeSpan ts = stopWatch.Elapsed;
 
                 FileInfo TS_File = new FileInfo(source_filename); // Load transferred file to get data
                 
-                    data_transfer = TS_File.Length / ts.TotalSeconds; //Transferred bytes by second
-                Console.WriteLine("Transfer Speed: {0}", data_transfer);
+                 data_transfer = (TS_File.Length / ts.TotalSeconds)/ 1000000; //Transferred MB/s
+                Console.WriteLine("Estimated Transfer Speed MB/s USB2.0: {0}", data_transfer);
             }
             catch (DirectoryNotFoundException dnfe)
             {
-                Console.WriteLine("Drive was not found!");
+                Console.WriteLine("ERROR: Drive was not found, Disconnected");
                 Console.WriteLine(dnfe.Message);
+                return dnfe.HResult;
             }
-            catch (FileNotFoundException)
+
+            catch (UnauthorizedAccessException dnfe)
+            {
+                Console.WriteLine(dnfe.Message); //File already copied
+                return dnfe.HResult;
+            }
+
+            catch (FileNotFoundException dnfe)
             {
                 Console.WriteLine("ERROR: File not found");
+                Console.WriteLine(dnfe.Message); //File already copied
+                return dnfe.HResult;
             }
+
+
+            try //Write a sample file to USB 3.0 and log the speed
+            {
+
+                dest_filename_USB3 = Path.Combine(USB3_Drives[usb3_drive], "TestFile.bin");
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                File.Copy(source_filename, dest_filename_USB3); //Copy the file
+                stopWatch.Stop();
+                // Get the elapsed time as a TimeSpan value.
+                TimeSpan ts = stopWatch.Elapsed;
+
+                FileInfo TS_File = new FileInfo(source_filename); // Load transferred file to get data
+
+                data_transfer = (TS_File.Length / ts.TotalSeconds) / 1000000; //Transferred MB/s
+                Console.WriteLine("Estimated Transfer Speed USB3.0: {0}", data_transfer);
+            }
+
+            catch (UnauthorizedAccessException dnfe)
+            {
+                Console.WriteLine(dnfe.Message); //File already copied
+                return dnfe.HResult;
+            }
+
+            catch (DirectoryNotFoundException dnfe)
+            {
+                Console.WriteLine("ERROR: Drive was not found or was disconnected");
+                Console.WriteLine(dnfe.Message);
+                return dnfe.HResult;
+            }
+            catch (FileNotFoundException dnfe)
+            {
+                Console.WriteLine("ERROR: File not found");
+                return dnfe.HResult;
+            }
+
+
+            // ReadingTest
+
+            FileInfo USB2_File = new FileInfo(dest_filename_USB2); // Load transferred file to get data
+
+            Console.WriteLine("Read file {0} from 2.0 Drive with Lenght {1}",USB2_File.Name, USB2_File.Length);
+
+
+            FileInfo USB3_File = new FileInfo(dest_filename_USB3);
+            Console.WriteLine("Read file {0} from 3.0 Drive with Lenght {1}", USB3_File.Name, USB3_File.Length);
+
+            //Cleanup and delete files from drive USB
+            File.Delete(dest_filename_USB2);
+            File.Delete(dest_filename_USB3);
+
+            return 0;
+
         }
+
+
 
         public int RandomNumber(int min, int max)
         {
