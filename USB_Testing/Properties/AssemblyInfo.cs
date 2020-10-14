48bc8191c1d9638be4e9aa883d87a5d34cc4a880
Alejandro Resendez Guerero <al_resendez@hotmail.com>
2020-10-14 12:48:27 -0500

    Add version number to Property Config

--- USB_Testing/Properties/AssemblyInfo.cs
+++ USB_Testing/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("USB_Testing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("USB Test Tool")]
+[assembly: AssemblyDescription("Console Application tool to run Removable Drive Tests attached to Host PC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("USB_Testing")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("ARGE Technologies, LLC")]
+[assembly: AssemblyProduct("USB Test Tool")]
+[assembly: AssemblyCopyright("Copyright 2020 ©")]
+[assembly: AssemblyTrademark("ARGE Software ®")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
